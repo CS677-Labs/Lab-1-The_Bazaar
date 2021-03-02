2129bec2b1777b69ecc6a6a9acef60645e93dc83
Brinda M <brinda.m95@gmail.com>
2021-03-02 12:35:41 -0500

    Modified restock logic to refill right after the last product is sold; Add check on product name

--- src/Seller.java
+++ src/Seller.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 // TODO: Test
 // TODO: refill when one item is over
+
 /**
  * Each seller picks one of three items to sell.
  * Each seller starts with m items (e.g., m boars) to sell; upon selling all m items, the seller picks another item at random and becomes a seller of that item.
@@ -11,15 +10,14 @@ import java.util.concurrent.Semaphore;
 // fixme What's restock forever? If you restock forever, the seller will never change the product right?
 
 public class Seller {
+    // TODO Fetch from a config file
+    private static final String[] items = new String[]{"Fish", "Boar", "Salt"};
     private final Semaphore semaphore;
     public String productName;
     public int nodeId;
     public int productCount;
-    // TODO Fetch from a config file
-    private static final String[] items = new String[]{"Fish", "Boar", "Salt"};
-
     //TODO Fetch from config file
-    private int maxProductCount = 50;
+    private final int maxProductCount = 50;
 
     public Seller(String productName, int nodeId) {
         this.semaphore = new Semaphore(1);
@@ -27,21 +25,26 @@ public class Seller {
         this.nodeId = nodeId;
     }
 
-    private void restock(){
+    private void restock() {
         Random random = new Random();
         this.productName = items[random.nextInt(items.length)];
         this.productCount = this.maxProductCount;
     }
-    private boolean sellProduct() {
+
+    private boolean sellProduct(String productName) {
+        if (!productName.equals(this.productName))
+            return false;
+
         boolean bought = false;
         try {
             this.semaphore.acquire();
             if (this.productCount >= 1) {
                 this.productCount -= 1;
                 bought = true;
+                if (this.productCount == 0)
+                    restock();
             }
-            else
-                restock();
+
         } catch (InterruptedException exc) {
             exc.printStackTrace();
         }

1781086f8c0f56c327126ccfd61776384a9e6d43
Brinda M <brinda.m95@gmail.com>
2021-03-02 00:15:49 -0500

    Add functions to pick a product to buy and restock when empty

--- src/Seller.java
+++ src/Seller.java
@@ -1,16 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.Semaphore;
 // TODO: Test
 // TODO: refill when one item is over
 /**
  * Each seller picks one of three items to sell.
- * Each buyer randomly picks an item and attempts to purchase it; it then waits a random amount of time, then picks another item to buy and so on.
  * Each seller starts with m items (e.g., m boars) to sell; upon selling all m items, the seller picks another item at random and becomes a seller of that item.
  */
+// fixme What's restock forever? If you restock forever, the seller will never change the product right?
+
 public class Seller {
     private final Semaphore semaphore;
     public String productName;
     public int nodeId;
     public int productCount;
+    // TODO Fetch from a config file
+    private static final String[] items = new String[]{"Fish", "Boar", "Salt"};
+
+    //TODO Fetch from config file
+    private int maxProductCount = 50;
 
     public Seller(String productName, int nodeId) {
         this.semaphore = new Semaphore(1);
@@ -18,6 +27,11 @@ public class Seller {
         this.nodeId = nodeId;
     }
 
+    private void restock(){
+        Random random = new Random();
+        this.productName = items[random.nextInt(items.length)];
+        this.productCount = this.maxProductCount;
+    }
     private boolean sellProduct() {
         boolean bought = false;
         try {
@@ -26,6 +40,8 @@ public class Seller {
                 this.productCount -= 1;
                 bought = true;
             }
+            else
+                restock();
         } catch (InterruptedException exc) {
             exc.printStackTrace();
         }

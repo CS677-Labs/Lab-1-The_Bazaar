b9085a4dd9c445bbdde556cb5e5dcc5231507558
Vignesh R <vigneshr@Vigneshs-MacBook-Pro.local>
2021-03-17 22:36:58 -0400

    Updated run.sh with latest remote execution changes

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-#/bin/bash
+#!/bin/bash
 set -e
 mkdir -p classfiles
 function finish {
   echo "Cleanup"
   rm -rf classfiles
-  rm -rf *.jar || grep "No jars to cleanup"
+  rm -rf *.jar || echo "No jars to cleanup"
   for i in "${local_pids[@]}"
   do
    kill $i || echo "Failed to kill $i. It is probably already killed."
@@ -12,12 +12,17 @@ function finish {
   done
 
 #  # For remote cleanup
-#  while IFS= read -r line
-#  do
-#    line=(${line//=/ })
-#    id=${line[0]}
-#    ssh root@$id "kill ${pids[id]}" || echo "Failed to kill process $i."
-#  done < "config-milestone3.properties"
+  while IFS= read -r line
+  do
+    newline=(${line//=/ })
+    id=${newline[0]}
+    network=(${newline[1]//,/ })
+    url=(${network[0]//:/ })
+    ip="${url[0]}:${url[1]}"
+    fullurl=$ip
+    ip=$(echo "$fullurl" |sed 's/https\?:\/\///')
+    ssh -n ec2-user@"$ip" "kill ${pids[id]}" || echo "Failed to kill process $i."
+  done < "network-config.properties"
   rm -rf build/* >/dev/null 2>&1
 }
 trap finish EXIT
@@ -38,44 +43,47 @@ python generateConfigFile.py $1 $2 $3 network-config.properties
 
 roles[1]="Client"
 roles[0]="Server"
+
+
 while IFS= read -r line
 do
-  line=(${line//=/ })
-  id=${line[0]}
-  network=(${line[1]//,/ })
+  newline=(${line//=/ })
+  id=${newline[0]}
+  network=(${newline[1]//,/ })
   url=(${network[0]//:/ })
   ip="${url[0]}:${url[1]}"
   port=${url[2]}
   role=${roles[id%2]}
   node_roles[id]=$role
-
   if [[ "$ip" == *"http://localhost" ]] || [[ "$ip" == *"http://127.0.0.1" ]]
   then
     echo "Running role $role on Localhost...."
     java -Djava.rmi.server.codebase=file:/ -jar ${role}.jar $id network-config.properties Fish 2 >/dev/null 2>&1 &
     pid=$!
     sleep 3
-    ps | grep "java" | grep "$pid" >/dev/null 2>&1
+    ps | grep "java" | grep "$pid" | grep -v grep >/dev/null 2>&1
     status=$?
     local_pids[id]=$pid
   else
+    fullurl=$ip
+    ip=$(echo "$fullurl" |sed 's/https\?:\/\///')
     echo "Running role $role on remote machine $ip."
     dir[id]="temp_$id"
-    ssh root@"$line" "rm -rf temp* && mkdir temp_$id"
+    ssh -n ec2-user@"$ip" "rm -rf temp_$id && mkdir temp_$id && git clone https://github.com/CS677-Labs/Lab-1-The_Bazaar.git || echo "Repo already present" && cd Lab-1-The_Bazaar && javac -d classfiles src/*.java && jar cfe Server.jar Server -C classfiles . && jar cfe Client.jar Client -C classfiles . && cp *.jar ../temp_$id"
     # Ensure there is at least one seller and one buyer in the system.
-    scp role.jar "config-milestone3.properties" root@"$ip":/"temp_$id"
+    scp "network-config.properties" ec2-user@"$ip":"temp_$id"
     # Todo: Run command differently for buyer and server.
-    pid=$(ssh root@$ip "java -Djava.rmi.server.codebase=file:/ -jar ${role}.jar $role $id network-config.properties Fish 2 >/dev/null 2>&1 & && echo $?")
+    pid=$(ssh -n ec2-user@$ip "cd temp_$id && (java -Djava.rmi.server.codebase=file:/ -jar ${role}.jar $id network-config.properties Fish 2 >/dev/null 2>&1 & echo \$!)")
     sleep 2
-    ssh root@"$line" "ps | grep \"java\" | grep \"$pid\" >/dev/null 2>&1"
-    status=$?
-    pids[id]=pid
+    status=0
+    ssh -n ec2-user@"$ip" "ps -ef | grep java | grep $pid | grep -v grep" || status=$?
+    pids[id]=$pid
   fi
   if [[ "$status" != 0 ]]
   then
 	  echo "Failed to start the node $id with ip $ip. Exiting..." && return 1
   fi
-done < "network-config.properties"
+done < network-config.properties
 
 echo "Press 'q' to exit"
 count=0
